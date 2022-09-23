819fb95bf31dc030276855b8ded9ab7de3b702b5
goodday <37609646+SungYoon-Jo@users.noreply.github.com>
2022-09-23 13:55:30 +0900

    sung

--- team/start/app.py
+++ team/start/app.py
@@ -1,12 +1,12 @@
 from PIL import Image, ImageDraw, ImageFont
                     #(가로, 세로)
-img = Image.new("RGBA", (100,100), 0)
+img = Image.new("RGBA", (100,100), 0) #워터마크 생성
 
 soimg = Image.open(f"./img/so/2.jpg")
 
 fnt = ImageFont.truetype("arial.ttf", 30)
 
-draw = ImageDraw.Draw(soimg)
+draw = ImageDraw.Draw(soimg) #원본 이미지
         #(좌표 : x, y)
 draw.text((0,0), "20220919", font=fnt, fill=(0,0,0))
 
